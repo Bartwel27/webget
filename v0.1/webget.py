95620fbb1e9147fa115af02ac0d5611cb214eeb5
Bartwel27 <81015407+Bartwel27@users.noreply.github.com>
2024-03-20 15:19:45 +0200

    Add files via upload

--- v0.1/webget.py
+++ v0.1/webget.py
@@ -5,7 +5,6 @@ def shellXecute(cmd,pcmd):
 		os.system(f"""
 		 if pip show {pcmd} -q
 		 then
-		   echo "Requirement already satisfied!"
 		 else
 		   pip install {pcmd} || pip2 install {pcmd}
 		 fi
@@ -15,7 +14,6 @@ def shellXecute(cmd,pcmd):
 		 if command -v {cmd} > log
 		 then
 		   rm -rf log
-		   echo "Requirement already satisfied!"
 		  else
 		   rm -rf log
 		   pkg install {cmd} || sudo apt install {cmd}
@@ -26,7 +24,6 @@ def shellXecute(cmd,pcmd):
 		 if pip show {pcmd} -q && command -v {cmd} > log
 		 then
 		   rm -rf log
-		   echo "Requirement already satisfied!"
 		 else
 		   rm -rf log
 		   pkg install {cmd} || sudo apt install {cmd}
@@ -47,21 +44,33 @@ from os.path import exists
 from time import sleep
 import requests
 
-link = input("link: ")
-file = input("filename: ")
 
-r = requests.get(link)
-with open(f"{file}","wb") as f:
-	f.write(r.content)
-	if r == r:
-		if exists(file):
-			print("...Done")
-		elif exists(file) == False:
-			os.system(f"wget {link}")
+def fileHundler():
+	link = input("link: ")
+	file = input("filename: ")
+
+	r = requests.get(link)
+	with open(f"{file}","wb") as f:
+		f.write(r.content)
+		if r == r:
 			if exists(file):
 				print("...Done")
-			if exists(file):
-				os.system(f"curl -o {file} {link}")
+			elif exists(file) == False:
+				os.system(f"wget {link}")
 				if exists(file):
-					print("..Done")
-		
+					print("...Done")
+				if exists(file):
+					os.system(f"curl -o {file} {link}")
+					if exists(file):
+						print("..Done")
+					else:
+						pass
+				else:
+					pass
+			else:
+				pass
+		else:
+			pass
+						
+while True:
+	fileHundler()
